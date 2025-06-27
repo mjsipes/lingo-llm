7960ebc715ff03dfa1bb4b3414fdff7cc8069882
Michael Sipes <msipes@usc.edu>
2025-06-27 10:32:18 -0700

    give edward and major 20 credits

--- lib/supabase/AuthProvider.tsx
+++ lib/supabase/AuthProvider.tsx
@@ -28,7 +28,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
       if (!user) {
         const { data, error } = await supabase.auth.signInAnonymously({
-          options: { data: { full_name: "guest", credits: 10 } },
+          options: { data: { full_name: "guest", credits: 20 } },
         });
         if (data?.user) setUser(data.user);
         if (error) console.error("Anonymous auth error:", error);
